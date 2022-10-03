86d4bfb8673ca498df9ec6b353b0d125c317d74b
Ma Yuheng <1075288776@qq.com>
2022-10-03 16:11:37 +0800

    update

--- summarize.py
+++ summarize.py
@@ -28,6 +28,34 @@ summary = pd.pivot_table(summarize_log, index=["dim", "method"],columns=["distri
 summary.to_excel("./sorted_result/simulation_summary.xlsx")
 
 
+# simulation l1 summarize
+log_file_dir = "./simulation_result_l1"
+method_seq = glob.glob("{}/*.csv".format(log_file_dir))
+
+method_seq = [os.path.split(method)[1].split('.')[0] for method in method_seq]
+
+print(method_seq)
+
+summarize_log=pd.DataFrame([])
+
+for method in method_seq:
+    
+    log = pd.read_csv("{}/{}.csv".format(log_file_dir,method), header=None)
+    
+    
+    log.columns = "distribution,dim,anll,L2,mae,mse,time,best_params,seed,n_train,n_test".split(',')
+    log["method"]=method
+    summarize_log=summarize_log.append(log)
+    
+    
+summary = pd.pivot_table(summarize_log, index=["dim", "method"],columns=["distribution"], values=[ "L2","mae"], aggfunc=[np.mean, np.std, len])
+
+summary.to_excel("./sorted_result/simulation_l1_summary.xlsx")
+
+
+
+
+
 # real data summarize
 log_file_dir = "./realdata_result"
 method_seq = glob.glob("{}/*.csv".format(log_file_dir))
