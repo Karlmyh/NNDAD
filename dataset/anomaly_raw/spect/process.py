cf533ce0f243b807c967ad7f364062dd2c460d19
karl <mayuhengkarl@gmail.com>
2023-09-02 09:32:05 +0800

    delete large file

--- dataset/anomaly_raw/spect/process.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-file_path = "./SPECTF.train"
-# above .data file is comma delimited
-data = pd.read_csv(file_path,delimiter=",",header=None)
-
-file_path = "./SPECTF.test"
-data=data.append(pd.read_csv(file_path,delimiter=",",header=None))
-data.to_csv('./heart.csv',index=None) 
